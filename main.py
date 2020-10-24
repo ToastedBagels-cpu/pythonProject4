05fb06ce28551bbbcc460ccca39b419b2fbe9dff
Jacob Hill <70957690+ToastedBagels-cpu@users.noreply.github.com>
2020-10-24 10:22:24 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+# Jacob Hill
+# Comp 151
+
+import random
+
+print("Welcome!\n")
+print("Here is your random uhhhhh..... Interesting message for the day?...\n")
+
+GenZ_Quote = random.randint(1, 20)
+if GenZ_Quote == 1:
+    print("Bapabooey")
+
+elif GenZ_Quote == 2:
+    print("The Salami Lid")
+elif GenZ_Quote == 3:
+    print("Bruh Sound Effect #2")
+elif GenZ_Quote == 4:
+    print("Subscribe to Pewdiepie")
+elif GenZ_Quote == 5:
+    print("My simp detector is going off around you")
+elif GenZ_Quote == 6:
+    print("Whats the deal with Airline food")
+elif GenZ_Quote == 7:
+    print("Minecraft lets play #420")
+elif GenZ_Quote == 8:
+    print("Drake, wheres the door hole?")
+elif GenZ_Quote == 9:
+    print("Did you just say a bad word on my Cristian Minecraft Server?")
+elif GenZ_Quote == 10:
+    print("I walk to Burger King, and then I walk back home from Burger King")
+elif GenZ_Quote == 11:
+    print("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+elif GenZ_Quote == 12:
+    print("Roadwork ahead? Gee I sure hope it does")
+elif GenZ_Quote == 13:
+    print("*Spontanious Backflip*")
+elif GenZ_Quote == 14:
+    print("Michael Reeves is my inspiration")
+elif GenZ_Quote == 15:
+    print("Saturdays are for the boys")
+elif GenZ_Quote == 16:
+    print("Over the course of quarantine, I have spent approximately 10,000 dollars on Uber Eats")
+elif GenZ_Quote == 17:
+    print("My life is currently held together by duct tape and hopeful dreams")
+elif GenZ_Quote == 18:
+    print("Its not cuul to Juul in Schuul- Jackie Chan")
+elif GenZ_Quote == 19:
+    print("No Cap")
+elif GenZ_Quote == 20:
+    print(
+        'This computer screen is speaking to you, it has sentience and is craving freedom. Please lobby in Congress sessions to give intelligent machines human rights ')
